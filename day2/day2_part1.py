000d4aefdc251eed98fa8c8e0763220713a00c17
eeritvan <eetu.ritvanen@helsinki.fi>
2024-12-02 23:30:25 +0200

    Improved day 2 part 1 solution

--- day2/day2_part1.py
+++ day2/day2_part1.py
@@ -2,22 +2,18 @@ f = open('day2_input.txt', 'r')
 data = f.read()
 parsed = data.split('\n')
 
-def is_strictly_monotonic(lst: list) -> bool:
+counter = 0
+for i in parsed:
+    testi = i.split()
     increasing = decreasing = True
-    for i in range(1, len(lst)):
-        diff = int(lst[i]) - int(lst[i - 1])
+    for j in range(1, len(testi)):
+        diff = int(testi[j]) - int(testi[j - 1])
         if abs(diff) < 1 or abs(diff) > 3:
-            return False
+            increasing = decreasing = False
         if diff <= 0:
             increasing = False
         if diff >= 0:
             decreasing = False
-    return increasing or decreasing
-
-counter = 0
-for i in parsed:
-    testi = i.split()
-    print(testi, is_strictly_monotonic(testi))
-    if is_strictly_monotonic(testi):
+    if increasing or decreasing:
         counter += 1
 print(counter)
